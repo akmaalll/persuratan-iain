5bbb994629f57cf7031aea9e5fe936160cc84f60
lili5777 <zailyanzali@gmail.com>
2024-09-23 19:37:31 +0800

    suratmasuk livewire2

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,11 +2,17 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-
     @livewireStyles
 </head>
 
 <body>
+
+
+
+    <main class="py-4">
+        {{ $slot }}
+    </main>
+
 
     @livewireScripts
 </body>
