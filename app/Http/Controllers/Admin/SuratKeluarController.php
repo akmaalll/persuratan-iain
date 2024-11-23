1bd4888d97804f1f90fbede1c26ebc33e35693b7
akmaalll <akmalrayadilfitrah@gmail.com>
2024-11-23 21:51:22 +0800

    fixing revisi

--- app/Http/Controllers/Admin/SuratKeluarController.php
+++ app/Http/Controllers/Admin/SuratKeluarController.php
@@ -36,7 +36,8 @@ class SuratKeluarController extends Controller
     {
         try {
             $title = $this->title;
-            $data = $this->repo->paginated($request->all());
+            $data = is_array($request->search) ? $this->repo->filter($request->all()) : $this->repo->paginated($request->all());
+            // $data = $this->repo->paginated($request->all());
             $perPage = $request->per_page == '' ? 5 : $request->per_page;
             $view = view('admin.' . $title . '.data', compact('data', 'title'))->with('i', ($request->input('page', 1) -
                 1) * $perPage)->render();
